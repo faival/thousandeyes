93aaf31081377cf1d23b02e3a4bbb37fdbd80ad0
faival <faival@gmail.com>
2015-10-04 16:43:58 +0200

    add jbcd

--- src/main/java/thousandeyes/TweeterController.java
+++ src/main/java/thousandeyes/TweeterController.java
@@ -8,21 +8,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TweeterController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
-	@RequestMapping("userTweets")
-	public String userTweets(@RequestParam(value="nick", defaultValue="") String nick, Model model) {
+	@RequestMapping("tweets")
+	@ResponseBody
+	public String tweets(@RequestParam(value="name", defaultValue="") String name, Model model) {
 		
-		model.addAttribute("userTweets", this.getTweetListForNick(nick));
-		return "userTeets";
+		model.addAttribute("tweets", this.getTweetListForName(name));
+		return model.toString();
 	}
 	
-	private List<TweetEntity> getTweetListForNick(String nick) {
-		TweetEntity oneTweet = new TweetEntity(counter.incrementAndGet(), String.format(template, nick));
+	private List<TweetEntity> getTweetListForName(String name) {
+		TweetEntity oneTweet = new TweetEntity(counter.incrementAndGet(), String.format(template, name));
 		List<TweetEntity> listOfTweets = new ArrayList<TweetEntity>();
 		listOfTweets.add(oneTweet);
 		return listOfTweets;
